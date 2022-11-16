d9d8289c75102f426ef4257a1c682e89fb36148c
Judge-Paul <judgepaulogebe3@gmail.com>
2022-11-16 12:12:42 +0100

    fix final bug patches for deployment

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,7 +8,7 @@ export default function Card(props) {
       <div>
         <div className="card-container">
             <div className="card-el mx-2 my-3 px-1">
-                <img src={`../../src/assets/images/cards/${props.img}`} alt="" />
+                <img src={`./cards/${props.img}`} alt="" />
                 <div className="d-flex pt-2">
                     <span className= "mr-auto">Desert king</span>
                     <span><b>1 MBT per night</b></span>
